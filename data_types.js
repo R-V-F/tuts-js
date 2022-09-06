ea186f5d000397039387eabf814c323aebef1799
R-V-F <100313927+R-V-F@users.noreply.github.com>
2022-09-05 23:43:24 -0300

    data_types 1

---
+++ data_types.js
@@ -0,0 +1,13 @@
+// ARRAYS are a kind of object!
+
+// For..in -> objects
+// For..of -> arrays
+
+//primitive types are 'wrapped' as objects to have access to methods
+//the object is later discarded -> highly optmized so the call isn't
+//super expensive
+
+//pop/push are cheap. shift/unshift are expensive
+
+//u can treat arrays as objs (ex: arr.isCool = true;), but this will
+//lead to bugs
